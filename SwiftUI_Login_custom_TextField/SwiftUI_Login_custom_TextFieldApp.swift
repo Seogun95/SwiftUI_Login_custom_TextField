910d74dbb3e0d39b6bd260876fd44ea1731484a4
SeonJungKim <ksj083895@gmail.com>
2021-06-14 20:07:13 +0900

    Initial Commit

---
+++ SwiftUI_Login_custom_TextField/SwiftUI_Login_custom_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Login_custom_TextFieldApp.swift
+//  SwiftUI_Login_custom_TextField
+//
+//  Created by Seogun Kim on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Login_custom_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
